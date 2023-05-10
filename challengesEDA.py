6b0aa47e70a0d1c22f21236588ff31b36abb1d3b
Derek Rogers <derekgrogers1@gmail.com>
2023-05-10 15:55:10 -0500

    Added EAD for challanges

---
+++ challengesEDA.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# Get the path to the survivorData directory
+data_dir = os.path.join(os.path.dirname(__file__), '..', 'survivorData')
+
+# List of CSV file names
+csv_files = [
+    'challenge_description.csv',
+    'challenge_results.csv',
+    'tribe_mapping.csv'
+]
+
+# Create a dictionary to store the DataFrames
+dataframes = {}
+
+# Loop through each CSV file and read its data into a DataFrame
+for csv_file in csv_files:
+    # Specify the relative path to the CSV file
+    file_path = os.path.join(data_dir, csv_file)
+    
+    # Read the data from the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_path)
+    
+    # Store the DataFrame in the dictionary
+    dataframes[csv_file] = df
+
+    # Print the head of the DataFrame
+    print(f'{csv_file}:')
+    print(df.describe())
+    
+# Count the number of TRUE values in each column from the 3rd column onward
+true_counts = (dataframes['challenge_description.csv'].iloc[:, 2:] == True).sum()
+
+# Count the number of TRUE values in each column from the 3rd column onward
+true_counts = (dataframes['challenge_description.csv'].iloc[:, 2:] == True).sum()
+
+# Create a bar chart showing the number of TRUE values in each column
+true_counts.plot(kind='bar')
+
+# Add labels and title
+plt.xlabel('Column')
+plt.ylabel('Number of challanges')
+plt.title('Number of each type of challange')
+
+# Show the plot
+plt.show()
