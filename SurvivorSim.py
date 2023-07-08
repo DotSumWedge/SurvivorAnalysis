190e3cfc5d90a30764bbb66d1b73bedf08a860ce
Derek Rogers <derekgrogers1@gmail.com>
2023-07-08 13:14:08 -0500

    Added .py file for development of the simulation logic

---
+++ SurvivorSim.py
@@ -0,0 +1,298 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn import preprocessing
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, LeaveOneGroupOut
+from sklearn.preprocessing import LabelEncoder
+from sklearn.exceptions import DataConversionWarning
+from scipy import stats
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import sys
+import warnings
+import math
+import os
+import random
+
+warnings.filterwarnings("ignore", category=DataConversionWarning)
+
+# Get the current working directory
+current_dir = os.getcwd()
+
+# Get the path to the survivorData directory
+data_dir = os.path.join(current_dir, '..', 'survivorData2')
+
+# List of CSV file names
+csv_files = [
+    'advantage_movement.csv',
+    'boot_mapping.csv',
+    'castaways.csv',
+    'castaway_details.csv',
+    'challenge_description.csv',
+    'challenge_results.csv',
+    'confessionals.csv',
+    'jury_votes.csv',
+    'screen_time.csv',
+    'season_palettes.csv',
+    'season_summary.csv',
+    'survivor_auction.csv',
+    'tribe_colours.csv',
+    'tribe_mapping.csv',
+    'viewers.csv',
+    'vote_history.csv'
+]
+
+# Create a dictionary to store the DataFrames
+dataframes = {}
+
+# Loop through each CSV file and read its data into a DataFrame
+for csv_file in csv_files:
+    # Specify the relative path to the CSV file
+    file_path = os.path.join(data_dir, csv_file)
+    
+    # Read the data from the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_path)
+    
+    # Store the DataFrame in the dictionary
+    dataframes[csv_file] = df
+    
+# Convert tribe_status column to category type
+dataframes['tribe_colours.csv']['tribe_status'] = dataframes['tribe_colours.csv']['tribe_status'].astype('category')
+
+# Convert result to a categorical variable
+dataframes['castaways.csv']['result'] = pd.Categorical(dataframes['castaways.csv']['result'])
+dataframes['castaways.csv']['result'] = dataframes['castaways.csv']['result'].cat.codes
+
+# Merge challenge_results and castaway_details dataframes on castaway_id
+castawayAll = pd.merge(dataframes['castaways.csv'], dataframes['castaway_details.csv'], on='castaway_id', how ="left")
+
+castawayAll['genderNumber'] = np.where(castawayAll['gender'] == 'Male', 1,
+                                     np.where(castawayAll['gender'] == 'Female', 2,
+                                              np.where(castawayAll['gender'] == 'Non-binary', 3, 0)))
+castawayAll['won'] = np.where(castawayAll['result'] == 'Sole Survivor', 1, 0)
+
+castawayAll = castawayAll.dropna(subset=['age'])
+
+# Read the challenge_results.csv file
+challenge_results = dataframes['challenge_results.csv']
+
+# Filter out the rows where challenge_type is "Immunity" and the result is "Won"
+immunity_wins = challenge_results[(challenge_results['challenge_type'] == 'Immunity') & (challenge_results['result'] == 'Won')]
+
+# Group by 'castaway_id' and count the number of immunity wins
+immunity_counts = immunity_wins.groupby('castaway_id').size()
+
+# Convert Series to DataFrame
+immunity_counts_df = immunity_counts.reset_index(name='immunityWins')
+
+# Set 'castaway_id' as the index for easier merging later
+immunity_counts_df.set_index('castaway_id', inplace=True)
+
+# Set 'castaway_id' as the index for 'castawayAll' dataframe for easier merging
+castawayAll.set_index('castaway_id', inplace=True)
+
+# Merge 'immunity_counts_df' into 'castawayAll'
+castawayAll = castawayAll.merge(immunity_counts_df, how='left', left_index=True, right_index=True)
+
+# Fill NaN values with 0 - assuming that contestants who didn't win any immunity challenges are not present in the immunity_counts_df DataFrame
+castawayAll['immunityWins'].fillna(0, inplace=True)
+
+# # Print out the column names
+# print("Columns in the DataFrame:")
+# print(castawayAll.columns)
+
+# # Print out how many people got each number of immunity wins
+# print("\nNumber of immunity wins for contestants:")
+# print(castawayAll['immunityWins'].value_counts())
+
+# # Print out details about the top 10 castaways with the most immunity wins
+# top_immunity_winners = castawayAll.nlargest(10, 'immunityWins')
+# print("\nTop 10 contestants with the most immunity wins:")
+# print(top_immunity_winners[['full_name_x', 'age', 'city', 'state', 'immunityWins']])
+
+# Once implemented, we can add this data to the x_train but x_test will have to be updated between each guess
+# # When creating your training and testing datasets, you should now include 'immunityWins' as a feature
+# x_train = pd.concat([season_split[i][['age', 'genderNumber', 'immunityWins']] for i in train_index])
+# x_test = pd.concat([season_split[i][['age', 'genderNumber', 'immunityWins']] for i in test_index])
+
+# Split dataframe into a list of dataframes grouped by column name
+def split_dataframe(df, column_name):
+    groups = df.groupby(column_name)
+    result = [group for _, group in groups]
+    return result
+
+# Call split_dataframe on column name "version_season"
+season_split = split_dataframe(castawayAll, 'version_season')
+
+# Give each contestant which order they were eliminated from the show
+for df in season_split:
+    df['orderOut'] = range(1, len(df) + 1)
+    
+def predict_elimination_order(df):
+    # Prepare the data
+    features = df[['age', 'genderNumber']]
+    target = df['orderOut']
+
+    # Initialize the model
+    #model = RandomForestRegressor()
+    model = RandomForestClassifier()
+    
+    # Train the model
+    model.fit(features, target)
+
+    # Make predictions
+    predictions = model.predict(features)
+
+    # Calculate the root mean square error (RMSE)
+    rmse = mean_squared_error(target, predictions, squared=False)
+
+    # print(f"Root Mean Square Error: {rmse}")
+    
+    # Return the order of elimination predictions
+    return predictions
+
+for i, df in enumerate(season_split):
+    # print(f"Predicting for season {i+1}")
+    predicted_order = predict_elimination_order(df)
+    # print(predicted_order)
+    
+# Use a random forest model that tries to predict the order the remaining contestants will be eliminated from the show.
+#       Return the contestant remaining that has the lowest orderOut prediction
+#       An orderOut value of 1 means that person was the first eleminated and the hightest orderOut in that season is the winner
+#       x_train is the age and gender of contestants in the training set
+#       y_train is the orderOut for contestants in the training set
+#       current_order is the orderOut value that is being predicted
+def person_prediction_random_forest(remaining_contestants, x_train_current, y_train_current, current_order):
+    # Train the random forest model
+    model = RandomForestClassifier()
+    model.fit(x_train_current, y_train_current)
+
+    # Predict the order for the remaining contestants
+    x_test_current = remaining_contestants[['age', 'genderNumber']]
+    predicted_order = model.predict(x_test_current)
+
+    # Find the contestants with the smallest predicted order
+    min_predicted_order = np.min(predicted_order)
+    contestants_with_min_order = np.where(predicted_order == min_predicted_order)[0]
+
+    # Randomly select one contestant from those with the smallest predicted order
+    person_predicted = np.random.choice(contestants_with_min_order)
+
+    return person_predicted
+
+def person_prediction_support_vector_machine(remaining_contestants, x_train_current, y_train_current, current_order):
+    # Train the support vector machien model using a "one-vs-rest" decision function shape
+    model = svm.SVC(decision_function_shape='ovo')
+    model.fit(x_train_current, y_train_current)
+
+    # Predict the order for the remaining contestants
+    x_test_current = remaining_contestants[['age', 'genderNumber']]
+    predicted_order = model.predict(x_test_current)
+
+    # Find the contestants with the smallest predicted order
+    min_predicted_order = np.min(predicted_order)
+    contestants_with_min_order = np.where(predicted_order == min_predicted_order)[0]
+
+    # Randomly select one contestant from those with the smallest predicted order
+    person_predicted = np.random.choice(contestants_with_min_order)
+
+    return person_predicted
+
+# NOT WORKING
+# This is run after each person is eliminated 
+def generate_challenge_features(challenge_results, current_season, current_episode):
+    # Filter the rows for the current season and episode
+    # current_results = challenge_results[(challenge_results['season'] == current_season) & (challenge_results['episode'] == current_episode)]
+    
+    # # Generate recent_challenge_result
+    # current_results['recent_challenge_result'] = current_results['result'].map({'Won': 1, 'Lost': 0})
+    
+    # # Generate total_wins_so_far
+    # total_wins = challenge_results[(challenge_results['season'] == current_season) & (challenge_results['episode'] <= current_episode) & (challenge_results['result'] == 'Won')].groupby('castaway_id').size()
+    # total_wins_df = total_wins.reset_index(name='total_wins_so_far')
+    
+    # # Merge recent_challenge_result and total_wins_so_far into current_results
+    # current_results = current_results.merge(total_wins_df, how='left', on='castaway_id')
+    # current_results['total_wins_so_far'].fillna(0, inplace=True)
+    
+    # return current_results[['castaway_id', 'recent_challenge_result', 'total_wins_so_far']]
+    return challenge_results
+
+# Perform leave-one-group-out cross-validation
+logo = LeaveOneGroupOut()
+
+# Create a list of group labels corresponding to each DataFrame in season_split
+group_labels = [i for i, _ in enumerate(season_split)]
+
+# print(len(season_split))
+# print(len(group_labels))
+
+accuracies_support_vector_machine = []  # Create an empty list to store accuracies
+
+for train_index, test_index in logo.split(season_split, groups=group_labels):
+
+    x_train = pd.concat([season_split[i][['age', 'genderNumber']] for i in train_index])
+    y_train = pd.concat([season_split[i][['orderOut']] for i in train_index])
+
+    x_test = pd.concat([season_split[i][['age', 'genderNumber']] for i in test_index])
+    y_test = pd.concat([season_split[i][['orderOut']] for i in test_index])
+
+    number_of_contestants = len(x_test)
+    order_out = 1
+    correct_predictions = 0
+
+    while len(x_test) > 0:
+        age_eliminated = x_test.iloc[0]['age']
+        gender_number_eliminated = x_test.iloc[0]['genderNumber']
+        
+        # Select a person
+        prediction_person_index = person_prediction_support_vector_machine(x_test, x_train, y_train, order_out)
+        
+        # print("-------------------------------------")
+        # print(f"Person prediction position: {prediction_person_index}")
+        # print(f"Person prediction age: {x_test.iloc[prediction_person_index]['age']}")
+        
+        # print(f"Person eleminated position: {prediction_person_index}")
+        # print(f"Person eleminated age: {age_eliminated}")
+        # print("-------------------------------------")
+        
+        # Check if the prediction is correct
+        if prediction_person_index == 0:
+            correct_predictions += 1
+        
+        # Remove the first element of x_test
+        x_test = x_test.iloc[1:]
+        order_out = order_out + 1
+        
+        current_season = 0
+        current_episode = 0
+        
+        # Update x_test to include updated challenge results
+        challenge_features = generate_challenge_features(x_test, current_season, current_episode)
+        # x_test = x_test.merge(challenge_features, how='left', on='castaway_id')
+
+    # Calculate and print the accuracy of the model
+    # print("----------")
+    # print(number_of_contestants)
+    accuracy = (correct_predictions / number_of_contestants) * 100
+    accuracies_support_vector_machine.append(accuracy)
+    # print(f"Model Accuracy: {accuracy}%")
+    # print("----------")
+    break
+    
+# print(x_test)
+# print(y_test)
+print(accuracies_support_vector_machine)
